7ecbf442df37496fc221e0f104593f2f56848458
sakib-code00 <asakibul.bd@gmail.com>
2024-02-20 16:56:55 +0600

    nothings added

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const seats = document.querySelectorAll(".seats");
 const seatCount = document.querySelector("#seatcount");
+const totalSeats = document.querySelector("#totalseat")
+const seatnoDiv = document.getElementById("seatno");
 let clickCount = 0;
+let totalseatCount = 0;
+
+
 for(let i = 0; i < seats.length; i++){
     const seat = seats[i];
     seat.addEventListener("click", function(){
         console.log("clicked");
 
         clickCount++;
+        totalseatCount++;
+        totalSeats.textContent = totalseatCount;
 
-            if (clickCount === 4) {
-                alert("You have Booked 4 seats");
-                seats.forEach(function(seat) {
-                    seat.style.pointerEvents = "none";
-                });
-            }
-
-
+        var seatInfoDiv = document.createElement("div");
+        seatInfoDiv.textContent = seat.textContent;
+        seatnoDiv.appendChild(seatInfoDiv);
 
         if(seat.classList.contains("bg-gray-300")){
             seat.classList.remove("bg-gray-300");
             seat.classList.add("bg-green-500");
         }
 
-        console.log(clickCount);
+        if (clickCount === 4) {
+            alert("You have Booked 4 seats");
+            seats.forEach(function(seat) {
+                seat.style.pointerEvents = "none";
+            });
+        }
+
+        
         var count = parseInt(seatCount.textContent);
         if (count > 0) {
             count--;
@@ -31,5 +40,6 @@ for(let i = 0; i < seats.length; i++){
         if (count === 0) {
             button.classList.add("pointer-events-none");
         }
+        
     })
 }

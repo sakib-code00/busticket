006363b1898637b5737302a20f6084262255c845
sakib-code00 <asakibul.bd@gmail.com>
2024-02-20 20:19:33 +0600

    assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
 const seats = document.querySelectorAll(".seats");
 const seatCount = document.querySelector("#seatcount");
 const totalSeats = document.querySelector("#totalseat")
 const seatnoDiv = document.getElementById("seatno");
+const totalSeatButton = document.getElementById("totalseat");
+const totalPriceElement = document.getElementById("totalPrice");
+let seatPrice = 550;
+let totalPrice = 0;
 let clickCount = 0;
 let totalseatCount = 0;
 
@@ -15,9 +20,19 @@ for(let i = 0; i < seats.length; i++){
         totalseatCount++;
         totalSeats.textContent = totalseatCount;
 
-        var seatInfoDiv = document.createElement("div");
+        let seatInfoDiv = document.createElement("div");
         seatInfoDiv.textContent = seat.textContent;
         seatnoDiv.appendChild(seatInfoDiv);
+
+
+        let totalSeatCount = parseInt(totalSeatButton.textContent);
+
+
+        totalPrice = totalSeatCount * seatPrice;
+
+
+        console.log("Total Price: BDT", totalPrice);
+        totalPriceElement.textContent = "BDT " + totalPrice;
 
         if(seat.classList.contains("bg-gray-300")){
             seat.classList.remove("bg-gray-300");
@@ -32,7 +47,7 @@ for(let i = 0; i < seats.length; i++){
         }
 
         
-        var count = parseInt(seatCount.textContent);
+        let count = parseInt(seatCount.textContent);
         if (count > 0) {
             count--;
             seatCount.textContent = count;
@@ -43,3 +58,24 @@ for(let i = 0; i < seats.length; i++){
         
     })
 }
+
+
+const mainSection = document.getElementById('mainSection');
+const nextButton = document.getElementById('nextButton');
+const bookingComplete = document.getElementById('bookingcomplete');
+const continueButton = document.getElementById('continueButton');
+
+nextButton.addEventListener("click", function(){
+    console.log("next button clicked");
+
+    mainSection.classList.add("hidden");
+    bookingComplete.classList.contains("hidden");
+    bookingComplete.classList.remove("hidden");
+});
+
+continueButton.addEventListener("click", function(){
+
+    mainSection.classList.remove("hidden");
+    bookingComplete.classList.add("hidden");
+});
+
